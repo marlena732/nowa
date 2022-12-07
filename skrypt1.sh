bb51c5f211ccd49a33110cad54bf7c6cc23ec332
marlena732 <marlenasliwa732@gmail.com>
2022-12-07 13:11:59 +0000

    update

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo "Dzien dobry FrontStepiaki!" 
-USER='marlena732'
-GIT='git'
-REPO_DIR=/home/m/firstgithube
-cd ${REPO_DIR}
-${GIT} add --all .
-${GIT} commit -m "Test commit"
-${GIT} push git@github.com:marlena732/nowa.git main
-exit 0
+
+cd " /home/m/firstgithube/nowa"
+
+git add -A
+git commit -am "update"
+git push
